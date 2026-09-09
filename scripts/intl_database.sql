057beb4a6f481bd4b38050463bc1917d56865c02
Jahnvi <singlajahnvi102@gmail.com>
2026-09-09 11:51:54 +0530

    Update intl_database.sql

--- scripts/intl_database.sql
+++ scripts/intl_database.sql
@@ -1,3 +1,23 @@
+/*
+===============================================================
+Create Database and Schemas
+===============================================================
+
+Script Purpose:
+    This script creates a new database named 'DataWarehouseAnalyst' after checking if it already exists.
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
+    within the database: 'bronze', 'silver', and 'gold'.
+
+WARNING:
+    Running this script will drop the entire 'DataWarehouseAnalyst' database if it exists.
+    All data in the database will be permanently deleted. Proceed with caution
+    and ensure you have proper backups before running this script.
+*/
+
+
+
+
+
 USE Master;
 Go
 IF EXISTS(Select 1 from sys.databases
